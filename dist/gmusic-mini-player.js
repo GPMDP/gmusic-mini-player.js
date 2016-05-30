d87c7f0fa4ad0661712320593ef2e9be26f38c38
Samuel Attard <samuel.r.attard@gmail.com>
2016-05-30 13:20:29 +1000

    Updated dist files

--- dist/gmusic-mini-player.js
+++ dist/gmusic-mini-player.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -21,13 +21,12 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
       CLASS_NAMESPACE: 'gmusic-mini-player'
     };
   }, {}], 2: [function (require, module, exports) {
-    // eslint-disable-line
 
     // DEV: These constants will be transformed into string constants by browserify
     var BASE_CSS = ".gmusic-mini-player #material-player-left-wrapper .now-playing-actions,.gmusic-mini-player body[controls] #mini-info,.gmusic-mini-player body[ready]:hover #mini-info{opacity:1!important}#embed-container,.gmusic-mini-player #mainContainer,.gmusic-mini-player #mini-info{overflow:hidden!important}.gmusic-mini-player,.gmusic-mini-player body{height:300px!important;width:300px!important}.gmusic-mini-player #player{min-width:280px!important;z-index:9999999!important;height:65px!important;min-height:65px!important;transition:bottom .3s!important;bottom:-100px!important;transition-timing-function:ease-in-out!important;width:280px!important;left:10px!important;right:10px!important;border-bottom-left-radius:8px!important;border-bottom-right-radius:8px!important}.gmusic-mini-player body[controls] #player,.gmusic-mini-player body[ready]:hover #player{bottom:-12px!important}.gmusic-mini-player #material-player-left-wrapper .now-playing-info-content,.gmusic-mini-player #material-player-left-wrapper .player-left .image-wrapper,.gmusic-mini-player #material-player-left-wrapper [data-id=now-playing-menu],.gmusic-mini-player #material-player-right-wrapper{display:none!important}.gmusic-mini-player #material-player-left-wrapper{flex:none!important;width:0!important}.gmusic-mini-player #player #material-player-left-wrapper .rating-container paper-icon-button{background:0 0!important;position:absolute!important;top:8px!important}.gmusic-mini-player #player #material-player-left-wrapper .rating-container paper-icon-button:nth-child(1){left:20px!important;display:none!important}.gmusic-mini-player #player #material-player-left-wrapper .rating-container paper-icon-button:nth-child(2){left:210px!important;display:none!important}.gmusic-mini-player #player[radio] #material-player-left-wrapper .rating-container paper-icon-button:nth-child(1),.gmusic-mini-player #player[radio] #material-player-left-wrapper .rating-container paper-icon-button:nth-child(2){display:block!important}.gmusic-mini-player #material-player-left-wrapper .rating-container{width:0!important}.gmusic-mini-player #mini-album{position:fixed!important;top:0!important;left:0!important;display:block!important;width:100%!important;height:100%!important;z-index:9999998!important}.gmusic-mini-player .player-progress-wrapper{left:0!important}.gmusic-mini-player .material-player-middle{margin:0 auto!important}.gmusic-mini-player #player.material .material-player-middle paper-icon-button[data-id=play-pause] iron-icon,.gmusic-mini-player #player.material .material-player-middle sj-icon-button[data-id=play-pause] core-icon{height:50px!important;width:50px!important;top:-2px!important}.gmusic-mini-player paper-icon-button[data-id=play-pause]{text-align:center!important}.gmusic-mini-player [data-id=play-pause]::shadow paper-ripple.circle{height:50px!important;width:50px!important;margin-left:6px!important;margin-top:-2px!important}.gmusic-mini-player [data-id=forward],.gmusic-mini-player [data-id=repeat],.gmusic-mini-player [data-id=rewind],.gmusic-mini-player [data-id=shuffle]{top:-8px!important}.gmusic-mini-player .mini-timespan-c,.gmusic-mini-player .mini-timespan-t{position:absolute!important;top:8px!important;font-size:11px!important;display:block!important}.gmusic-mini-player [data-id=repeat]{left:28px!important}.gmusic-mini-player [data-id=rewind]{left:16px!important}.gmusic-mini-player [data-id=forward]{left:-16px!important}.gmusic-mini-player [data-id=shuffle]{left:-28px!important}.gmusic-mini-player .mini-timespan-c{left:6px!important}.gmusic-mini-player .mini-timespan-t{right:6px!important}.gmusic-mini-player ::shadow #mainContainer{overflow:hidden!important}.gmusic-mini-player #mini-info{position:fixed!important;top:0!important;left:0!important;width:100%!important;height:90px!important;background:-webkit-linear-gradient(top,rgba(45,45,45,.85) 0,rgba(9,9,9,.41) 79%,rgba(0,0,0,0) 100%)!important;display:block!important;z-index:9999999!important;transition:opacity .3s!important;transition-timing-function:ease-in-out!important;opacity:0!important}.gmusic-mini-player #mini-info span{color:#EEE!important;display:block!important;font-size:20px!important;padding:4px 12px!important;cursor:default!important}.gmusic-mini-player #mini-info span:last-child{font-size:16px!important;height:18px!important;overflow:hidden!important}[data-id=show-miniplayer-dp]{color:#9e9e9e!important;position:absolute!important;top:auto!important;bottom:-4px!important;right:-4px!important;margin:0!important}#player paper-icon-button[data-id=show-miniplayer-dp] iron-icon{width:16px!important;height:16px!important}#embed-container{width:0!important}"; // eslint-disable-line
     var CONSTANTS = require('../lib/_constants');
 
-    var GMusicMiniPlayerController = function () {
+    var GMusicMiniPlayerController = (function () {
       _createClass(GMusicMiniPlayerController, [{
         key: "_initCSS",
         value: function _initCSS() {
@@ -188,7 +187,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
         this._initMiniPlayerRadioMonitor();
 
         this.miniState = false;
-        this.scrollVolume = false;
+        this.scrollVolume = true;
         this.events = {};
 
         this._initGlobalEventHandlers();
@@ -308,7 +307,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
       }]);
 
       return GMusicMiniPlayerController;
-    }();
+    })();
 
     if (!window.GMusic) {
       console.error('The core GMusic library must be included for the GMusic mini player library to work'); // eslint-disable-line
@@ -316,9 +315,6 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
         var controller = new GMusicMiniPlayerController();
         // DEV: Hook into the existing GMusic libraries global prototype
         window.GMusic._protoObj.mini = controller.getControls();
-        window.GMusic._protoObj.mini.on('miniScrollVolume', function (state) {
-          window.GMusic.mini.setScrollVolume(state);
-        });
       }
   }, { "../lib/_constants": 1 }] }, {}, [2]);
 //# sourceMappingURL=gmusic-mini-player.js.map
